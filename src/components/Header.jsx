59278134945717e77f7264f2128d48b4c4b88482
Mehmet <mathdenizi@gmail.com>
2024-07-04 23:35:36 +0200

    stop the project

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="hero">
+        <div className="hero-banner">
+          <h1>Game of Thrones</h1>
+          <p>
+            When people ask you what happened here, tell them the North
+            remembers{" "}
+          </p>
+          <a href="#" className="hro-btn">
+            Characters
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
